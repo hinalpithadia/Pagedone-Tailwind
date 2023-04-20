039ced490048ad9868b5cf3369ad92574b7a4b78
hinalpithadia <38879377+hinalpithadia@users.noreply.github.com>
2023-04-20 15:37:13 +0530

    Pagedone tailwind project

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './dist/**/*.{html,js}',
+    './dist/index.html',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        manrope: ['Manrope', 'sans-serif'],
+        inter: ['Inter', 'serif'],
+      },
+    },
+  },
+  plugins: [
+    require('tailwind-scrollbar'),
+  ],
+}
+
